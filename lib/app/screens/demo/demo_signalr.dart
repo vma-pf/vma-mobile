4707220192ba08ed05b0717a0fe7fa3dcc799db9
duykasama <duykasama@gmail.com>
2024-09-15 18:34:38 +0700

    feat: add signalr

---
+++ lib/app/screens/demo/demo_signalr.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:signalr_netcore/signalr_client.dart';
+
+class DemoSignalr extends StatefulWidget {
+  const DemoSignalr({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _DemoSignalr();
+}
+
+class _DemoSignalr extends State<DemoSignalr> {
+  late final HubConnection _connection;
+  static const String _serverUrl = 'http://localhost:5000/hub';
+
+  @override
+  void initState() {
+    super.initState();
+    _connectToHub();
+  }
+
+  void _connectToHub() async {
+    _connection = HubConnectionBuilder().withUrl(_serverUrl).build();
+    await _connection.start();
+    _listenToEvents();
+  }
+
+  void _listenToEvents() {
+    _connection.on('ReceiveMessage', (message) {
+      // Handle received message
+    });
+  }
+
+  void _invokeServerMethod() async {
+    await _connection.invoke('SendMessage', args: <Object>['Hello, World!']);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+            onPressed: _invokeServerMethod, child: const Text('Send message')),
+      ),
+    );
+  }
+}
