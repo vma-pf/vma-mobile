86a26b2b424892c2a4f20edc59133cbdc67baf58
duykasama <duykasama@gmail.com>
2024-10-27 11:11:08 +0700

    feat: display schedules and schedule details

--- lib/app/screens/vaccination_schedules/screen.dart
+++ lib/app/screens/vaccination_schedules/screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:vma/app/common/vma_state.dart';
+import 'package:vma/app/screens/vaccination_schedules/widgets/schedule_details.dart';
+import 'package:vma/core/view_models/vaccination_schedules_model.dart';
+import 'package:vma/core/models/vaccination_schedule.dart' as models;
 
 class VaccinationSchedule extends StatefulWidget {
   const VaccinationSchedule({super.key});
@@ -10,7 +14,9 @@ class VaccinationSchedule extends StatefulWidget {
 }
 
 class _VaccinationScheduleState extends VMAState<VaccinationSchedule> {
-  late final ValueNotifier<List<Event>> _selectedEvents;
+  final VaccinationSchedulesModel _model = VaccinationSchedulesModel();
+
+  late final ValueNotifier<List<models.VaccinationSchedule>> _selectedEvents;
   CalendarFormat _calendarFormat = CalendarFormat.month;
   RangeSelectionMode _rangeSelectionMode = RangeSelectionMode
       .toggledOff; // Can be toggled on/off by longpressing a date
@@ -24,19 +30,14 @@ class _VaccinationScheduleState extends VMAState<VaccinationSchedule> {
     super.initState();
 
     _selectedDay = _focusedDay;
-    _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
+    _selectedEvents = ValueNotifier(_model.getSchedulesByDay(_selectedDay!));
+    _model.loadVaccinationSchedules();
   }
 
   @override
   void dispose() {
     _selectedEvents.dispose();
     super.dispose();
-  }
-
-  List<Event> _getEventsForDay(DateTime day) {
-    // Implementation example
-    // return kEvents[day] ?? [];
-    return [];
   }
 
   List<DateTime> daysInRange(DateTime first, DateTime last) {
@@ -47,12 +48,15 @@ class _VaccinationScheduleState extends VMAState<VaccinationSchedule> {
     );
   }
 
-  List<Event> _getEventsForRange(DateTime start, DateTime end) {
+  List<models.VaccinationSchedule> _getEventsForRange(
+    DateTime start,
+    DateTime end,
+  ) {
     // Implementation example
     final days = daysInRange(start, end);
 
     return [
-      for (final d in days) ..._getEventsForDay(d),
+      for (final d in days) ..._model.getSchedulesByDay(d),
     ];
   }
 
@@ -66,7 +70,7 @@ class _VaccinationScheduleState extends VMAState<VaccinationSchedule> {
         _rangeSelectionMode = RangeSelectionMode.toggledOff;
       });
 
-      _selectedEvents.value = _getEventsForDay(selectedDay);
+      _selectedEvents.value = _model.getSchedulesByDay(selectedDay);
     }
   }
 
@@ -83,9 +87,9 @@ class _VaccinationScheduleState extends VMAState<VaccinationSchedule> {
     if (start != null && end != null) {
       _selectedEvents.value = _getEventsForRange(start, end);
     } else if (start != null) {
-      _selectedEvents.value = _getEventsForDay(start);
+      _selectedEvents.value = _model.getSchedulesByDay(start);
     } else if (end != null) {
-      _selectedEvents.value = _getEventsForDay(end);
+      _selectedEvents.value = _model.getSchedulesByDay(end);
     }
   }
 
@@ -95,74 +99,62 @@ class _VaccinationScheduleState extends VMAState<VaccinationSchedule> {
       appBar: AppBar(
         title: const Text('Lịch tiêm phòng'),
       ),
-      body: Column(
-        children: [
-          TableCalendar<Event>(
-            firstDay: DateTime.now(),
-            lastDay: DateTime.now().add(const Duration(days: 15)),
-            focusedDay: _focusedDay,
-            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
-            rangeStartDay: _rangeStart,
-            rangeEndDay: _rangeEnd,
-            calendarFormat: _calendarFormat,
-            rangeSelectionMode: _rangeSelectionMode,
-            eventLoader: _getEventsForDay,
-            startingDayOfWeek: StartingDayOfWeek.monday,
-            calendarStyle: CalendarStyle(
-              // Use `CalendarStyle` to customize the UI
-              outsideDaysVisible: false,
-            ),
-            onDaySelected: _onDaySelected,
-            onRangeSelected: _onRangeSelected,
-            onFormatChanged: (format) {
-              if (_calendarFormat != format) {
-                setState(() {
-                  _calendarFormat = format;
-                });
-              }
-            },
-            onPageChanged: (focusedDay) {
-              _focusedDay = focusedDay;
-            },
-          ),
-          const SizedBox(height: 8.0),
-          Expanded(
-            child: ValueListenableBuilder<List<Event>>(
-              valueListenable: _selectedEvents,
-              builder: (context, value, _) {
-                return ListView.builder(
-                  itemCount: value.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 12.0,
-                        vertical: 4.0,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                      child: ListTile(
-                        onTap: () => print('${value[index]}'),
-                        title: Text('${value[index]}'),
-                      ),
-                    );
+      body: ScopedModel(
+        model: _model,
+        child: ScopedModelDescendant<VaccinationSchedulesModel>(
+          builder: (context, child, model) {
+            return Column(
+              children: [
+                TableCalendar<models.VaccinationSchedule>(
+                  firstDay: _model.firstDate,
+                  lastDay: _model.lastDate,
+                  focusedDay: _focusedDay,
+                  selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+                  rangeStartDay: _rangeStart,
+                  rangeEndDay: _rangeEnd,
+                  calendarFormat: _calendarFormat,
+                  rangeSelectionMode: _rangeSelectionMode,
+                  eventLoader: _model.getSchedulesByDay,
+                  startingDayOfWeek: StartingDayOfWeek.monday,
+                  calendarStyle: const CalendarStyle(
+                    // Use `CalendarStyle` to customize the UI
+                    outsideDaysVisible: false,
+                  ),
+                  onDaySelected: _onDaySelected,
+                  onRangeSelected: _onRangeSelected,
+                  onFormatChanged: (format) {
+                    if (_calendarFormat != format) {
+                      setState(() {
+                        _calendarFormat = format;
+                      });
+                    }
                   },
-                );
-              },
-            ),
-          ),
-        ],
+                  onPageChanged: (focusedDay) {
+                    _focusedDay = focusedDay;
+                  },
+                ),
+                const SizedBox(height: 8.0),
+                Expanded(
+                  child:
+                      ValueListenableBuilder<List<models.VaccinationSchedule>>(
+                    valueListenable: _selectedEvents,
+                    builder: (context, value, _) {
+                      return ListView.builder(
+                        itemCount: value.length,
+                        itemBuilder: (context, index) {
+                          final models.VaccinationSchedule schedule =
+                              value[index];
+                          return ScheduleDetails(schedule: schedule);
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
-}
-
-class Event {
-  final String title;
-
-  const Event(this.title);
-
-  @override
-  String toString() => title;
 }
---
+++ lib/core/repositories/vaccination_schedule_repository.dart
@@ -0,0 +1,24 @@
+import 'package:vma/core/extensions/json_to_list_extension.dart';
+import 'package:vma/core/models/vaccination_schedule.dart';
+import 'package:vma/core/network/api.dart';
+
+class VaccinationScheduleRepository {
+  Future<List<VaccinationSchedule>> getVaccinationSchedules() async {
+    final result = await ApiCaller.instance.request(
+      path: '/api/my-schedule',
+      method: ApiMethod.get,
+    );
+
+    List<VaccinationSchedule> schedules = [];
+
+    result.either((success) {
+      final data = success.data as List<dynamic>;
+      schedules = data.fromJsonToList<VaccinationSchedule>(
+          (e) => VaccinationSchedule.fromJson(e));
+    }, (error) {
+      // handle error
+    });
+
+    return Future.value(schedules);
+  }
+}
